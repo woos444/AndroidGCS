0d6c4803f9df7e5aaed139b25690621b427351b3
mrvincent <vincent@metarobotics.com>
2019-07-02 22:53:01 +0900

    add new module

---
+++ mygcs/src/main/java/com/example/mygcs/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.mygcs;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
