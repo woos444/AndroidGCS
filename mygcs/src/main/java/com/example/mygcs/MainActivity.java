919e41d7f4dd931e7ba363bc78058e9105f8aa74
mrvincent <vincent@metarobotics.com>
2019-07-04 00:27:05 +0900

    modified navermap

--- mygcs/src/main/java/com/example/mygcs/MainActivity.java
+++ mygcs/src/main/java/com/example/mygcs/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.mygcs;
 
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.naver.maps.map.MapFragment;
+
 public class MainActivity extends AppCompatActivity {
+
+    MapFragment mNaverMapFragment = null;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FragmentManager fm = getSupportFragmentManager();
+        mNaverMapFragment = (MapFragment)fm.findFragmentById(R.id.map);
+        if (mNaverMapFragment == null) {
+            mNaverMapFragment = MapFragment.newInstance();
+            fm.beginTransaction().add(R.id.map, mNaverMapFragment).commit();
+        }
     }
 }
